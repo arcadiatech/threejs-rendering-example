c30fbf93653c5568b02b04556ff0d4b3161d51da
franco-martucci <franco.martucci.21@gmail.com>
2023-05-11 18:38:24 -0300

    Add cabin and trees

--- src/createThreeApp.js
+++ src/createThreeApp.js
@@ -1,10 +1,12 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import {
-  createBasicMesh,
+  //createBasicMesh,
+  createCabin,
   createCamera,
   createGround,
   createRenderer,
+  createTree,
 } from "./utils";
 import { animate } from "./animate";
 
@@ -20,11 +22,23 @@ export function createThreeApp(canvas) {
   const ambientLight = new THREE.AmbientLight("#556", 2.4);
   scene.add(ambientLight);
 
-  const mesh = createBasicMesh();
-  scene.add(mesh);
+  //const mesh = createBasicMesh();
+  //scene.add(mesh);
 
   const ground = createGround();
   scene.add(ground);
+
+  const trees = Array.from(Array(3), () => createTree());
+  trees[0].position.set(-3, 0, 6);
+  trees[1].position.set(-12, 0, 5);
+  trees[2].position.set(1, 0, -6);
+  scene.add(...trees);
+
+  const cabin = createCabin();
+  cabin.position.set(-2, 0, 0);
+  cabin.scale.multiplyScalar(0.8);
+  cabin.rotateY(Math.PI / 8);
+  scene.add(cabin);
 
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true;
--- src/utils.js
+++ src/utils.js
@@ -36,3 +36,67 @@ export function createGround() {
   ground.rotateX(-Math.PI / 2);
   return ground;
 }
+
+export function createTree() {
+  const greenMaterial = new THREE.MeshLambertMaterial({
+    color: "#285430",
+  });
+
+  const top = new THREE.Mesh(
+    new THREE.CylinderGeometry(0.1, 1.6, 3),
+    greenMaterial
+  );
+
+  const base = new THREE.Mesh(
+    new THREE.CylinderGeometry(1, 2.7, 4),
+    greenMaterial
+  );
+
+  const trunk = new THREE.Mesh(
+    new THREE.CylinderGeometry(1, 1, 2),
+    new THREE.MeshLambertMaterial({
+      color: "#371B58",
+    })
+  );
+
+  top.position.set(0, 3 / 2 + 5, 0);
+  base.position.set(0, 3 / 2 + 2, 0);
+  trunk.position.set(0, 1, 0);
+
+  const tree = new THREE.Group();
+  tree.add(trunk, base, top);
+  tree.scale.multiplyScalar(Math.random() / 2 + 0.75);
+  return tree;
+}
+
+export function createCabin() {
+  const cabin = new THREE.Group();
+
+  const wallMaterial = new THREE.MeshLambertMaterial({
+    color: "#2C3333",
+  });
+
+  const box = new THREE.Mesh(new THREE.BoxGeometry(3, 2, 5), wallMaterial);
+
+  const chimney = new THREE.Mesh(
+    new THREE.BoxGeometry(0.75, 6, 0.5),
+    wallMaterial
+  );
+
+  const roofRadius = 2;
+  const roof = new THREE.Mesh(
+    new THREE.CylinderGeometry(roofRadius, roofRadius, 5.4, 3),
+    new THREE.MeshLambertMaterial({
+      color: "#FC4F00",
+    })
+  );
+
+  box.position.set(0, 1, 0);
+  chimney.position.set(0, 3, 2);
+  roof.rotateZ(Math.PI);
+  roof.rotateX(Math.PI / 2);
+  roof.position.set(0, 2 + roofRadius / 2, 0); // this is generally not a correct formula for cylinders, unless it's a 3 sided cylinder
+
+  cabin.add(box, roof, chimney);
+  return cabin;
+}
